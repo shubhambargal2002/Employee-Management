edd7136e2f0c6109b8be5658820c43d61c66f9f9
shubhambargal2002 <shubhambargal2002@gmail.com>
2025-04-11 10:48:37 +0530

    done with login ui and installed mantine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
+import { Button } from "@mantine/core";
 
 function App() {
+  const [isLoading, setLoading] = useState(false);
+
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    callApi(formValues);
+  };
+
+  const callApi = (data) => {
+    console.log("data", data);
+    setLoading(false);
+  };
   return (
-    <div>
-      <h1>Employee Management</h1>
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <p className="heading">Login</p>
+        <div className="label_container">
+          <label htmlFor="email">
+            Email <span style={{ color: "red" }}>*</span>
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter email"
+            required
+            value={formValues.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="label_container">
+          <label htmlFor="password">
+            Password <span style={{ color: "red" }}>*</span>
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter password"
+            required
+            value={formValues.name}
+            onChange={handleChange}
+          />
+        </div>
+
+        <Button type="submit" loading={isLoading}>
+          Login
+        </Button>
+      </form>
     </div>
   );
 }
